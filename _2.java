5bd2cc833f688b124ff0555aeb16f614f9e6c9d4
surya <122369945+suryaprasadyadav@users.noreply.github.com>
2024-09-04 15:59:04 +0530

    Linear time Complexity

--- _2.java
+++ _2.java
@@ -1,11 +1,18 @@
-// Linear time complexity  O(n)
-
 public class _2 {
     public static void main(String[] args) {
         int[] arr = new int [] {1,2,3,4,5,6};
 
-        for (int i = 0; i < arr.length; i++) {// O(n) 
+        for (int i = 0; i < arr.length; i++) { // O(n)
             System.out.println(arr[i]);
+        }
+
+        int[] newarr = new int[arr.length - 1];
+        for (int i = 0; i < newarr.length; i++) {
+            newarr[i] = arr[i]; // Copying elements from arr to newarr so the space complexity is also O(n) as it depends on the size of inputed array
+        }
+
+        for (int i = 0; i < newarr.length; i++) {
+            System.out.println("newarr[" + i + "]=" + newarr[i]);
         }
     }
 }
