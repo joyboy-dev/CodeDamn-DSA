e095f3cb9a0812037ecd01e1bde3b1fc5530bb66
surya <122369945+suryaprasadyadav@users.noreply.github.com>
2024-09-14 14:11:40 +0530

    BinaryToDecimal with bitwise operation

--- _9.java
+++ _9.java
@@ -6,6 +6,7 @@ public class _9 {
         // System.out.println(c.convertBinaryToDecimal(0));
         c.convertDecimalToBinary(10);
         c.convertBinaryToDecimal(1010);
+        c.convertBinaryToDecimalUsingBitwiseOperation(1010);
     }
     // Binary to decimal
 
@@ -23,22 +24,40 @@ class Converter {
                 result = (num & 1) + result;
                 num = num >> 1;
             }
-            System.out.println("The Binary Representation of " + n + " = " + result+"");
+            System.out.println("The Binary Representation of " + n + " = " + result + "");
             // return Integer.parseInt(result);
         }
     }
 
     public static void convertBinaryToDecimal(int num) {
         String val = String.valueOf(num);
-        double result = 0;
+        int result = 0;
         for (int i = 0; i < val.length(); i++) {
-            if (val.charAt(val.length()-1-i) == '1') {
-                result = result + Math.pow(2, i);
+            if (val.charAt(val.length() - 1 - i) == '1') {
+                result = result + (int) Math.pow(2, i);
             }
 
         }
-        System.out.println("The Decimal Representation of " + val + " = " + result);
+        System.out.println("The Decimal Representation of " + val + " = " +  result);
 
+    }
+
+    public static void convertBinaryToDecimalUsingBitwiseOperation(int num) {
+        int n = num;
+        int result = 0;
+        int digit = 0;
+        int i = 0;
+        while (num != 0) {
+
+            digit = num % 10;
+            if (digit == 1) {
+                result = (int) Math.pow(2, i) + result;
+            }
+            num /= 10;
+            i++;
+        }
+
+        System.out.println("The Decimal Representation of " + n + " = " + result);
     }
 
 }
